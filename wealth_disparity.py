1fa3003f3e63927c39b54c9465f2b7e1a2bbb7a8
ArtistBanda <chirag.11gomber4@gmail.com>
2018-09-14 10:41:48 +0530

    description modified

--- wealth_disparity.py
+++ wealth_disparity.py
@@ -3,9 +3,16 @@
 
 Indian National Olympiad in Informatics, 2016
 
-Boing Inc, has N employees, numbered 1 … N. Every employee other than Mr. Hojo (the head of the company) has a manager (P[i] denotes the manager of employee i). Thus an employee may manage any number of other employees but he reports only to one manager, so that the organization forms a tree with Mr. Hojo at the root. We say that employee B is a subordinate of employee A if B appears in the subtree rooted at A.
+Boing Inc, has N employees, numbered 1 … N. Every employee other than Mr. Hojo (the head of the company) has a manager (P[i] denotes the manager of employee i).
+Thus an employee may manage any number of other employees but he reports only to one manager, so that the organization forms a tree with Mr. Hojo at the root.
+ We say that employee B is a subordinate of employee A if B appears in the subtree rooted at A.
 
-Mr. Hojo, has hired Nikhil to analyze data about the employees to suggest how to identify faults in Boing Inc. Nikhil, who is just a clueless consultant, has decided to examine wealth disparity in the company. He has with him the net wealth of every employee (denoted A[i] for employee i). Note that this can be negative if the employee is in debt. He has already decided that he will present evidence that wealth falls rapidly as one goes down the organizational tree. He plans to identify a pair of employees i and j, j a subordinate of i, such that the wealth difference A[i] - A[j] is maximum. Your task is to help him do this.
+Mr. Hojo, has hired Nikhil to analyze data about the employees to suggest how to identify faults in Boing Inc. Nikhil,
+who is just a clueless consultant, has decided to examine wealth disparity in the company.
+He has with him the net wealth of every employee (denoted A[i] for employee i).
+Note that this can be negative if the employee is in debt. He has already decided that he will present evidence that wealth falls rapidly
+as one goes down the organizational tree. He plans to identify a pair of employees i and j, j a subordinate of i, such that the wealth difference A[i] - A[j] is maximum.
+Your task is to help him do this.
 
 Suppose, Boing Inc has 4 employees and the parent (P[i]) and wealth information (A[i]) for each employee are as follows:
 
@@ -21,7 +28,9 @@ Solution hint
 The company hierarchy is a tree. Explore using DFS and incrementally keep track of the maximum difference. Your algorithm should work in time O(n+m).
 Input Format
 
-There are three lines of input. The first line contains a single integer N, giving the number of employees in the company. The next line has N integers A[1], .., A[N] that give the wealth of the N employees. The third line has N integers P[1], P[2], .., P[N], where P[i] identifies the manager of employee i. If Mr. Hojo is employee i then, P[i] = -1, indicating that he has no manager.
+There are three lines of input. The first line contains a single integer N, giving the number of employees in the company.
+ The next line has N integers A[1], .., A[N] that give the wealth of the N employees. The third line has N integers P[1], P[2], .., P[N],
+  where P[i] identifies the manager of employee i. If Mr. Hojo is employee i then, P[i] = -1, indicating that he has no manager.
 Output Format
 
 One integer, which is the needed answer. """
