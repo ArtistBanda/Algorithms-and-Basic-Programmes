7c35694ce9612b24e8e281f71a884e00f1c7bce5
NeeharikaESai <43785833+NeeharikaESai@users.noreply.github.com>
2019-10-28 01:54:27 +0530

    Update bubbleSort.cpp

--- CPP/Algorithms/bubbleSort.cpp
+++ CPP/Algorithms/bubbleSort.cpp
@@ -1,35 +1,31 @@
-#include <iostream>
+#include<iostream>
+ 
 using namespace std;
-
-void bubbleSort(int arr[], int n)
-{
-    int i, j, temp = 0;
-    for (i = 0; i < n - 1; i++)
-    {
-        for (j = 0; j < n - i - 1; j++)
-        {
-            if (arr[j] > arr[j + 1])
-            {
-                temp = arr[j + 1];
-                arr[j + 1] = arr[j];
-                arr[j] = temp;
-            }
-        }
-    }
-}
-
-// Test Code
-
+ 
 int main()
 {
-    int i, n, ar[100];
-    cin >> n;
-    for (i = 0; i < n; i++)
-    {
-        cin >> ar[i];
-    }
-    bubbleSort(ar, n);
-    for (int i = 0; i < n; i++)
-        cout << ar[i] << ' ';
-    return 0;
+	int a[50],n,i,j,temp;
+	cout<<"Enter the size of array: ";
+	cin>>n;
+	cout<<"Enter the array elements: "; 
+	
+	for(i=0;i<n;++i)
+		cin>>a[i];
+		
+	for(i=1;i<n;++i)
+	{
+		for(j=0;j<(n-i);++j)
+			if(a[j]>a[j+1])
+			{
+				temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+			}
+	}
+	
+	cout<<"Array after bubble sort:";
+	for(i=0;i<n;++i)
+		cout<<" "<<a[i];
+		
+	return 0;
 }
