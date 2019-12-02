b75910815a46595030062b3d067be613e3aac45f
ArtistBanda <chirag.11gomber4@gmail.com>
2019-12-02 20:29:01 +0530

    Updated Fibonacci.cpp with euclid theorem

--- CPP/Algorithms/Fibonacci.cpp
+++ CPP/Algorithms/Fibonacci.cpp
@@ -10,9 +10,24 @@ long long fibonacci_naive(int value)
         return fibonacci_naive(value - 1) + fibonacci_naive(value - 2);
 }
 
+long long fibonacci_euclid(int value, long long arr[])
+{
+    if (arr[value])
+        return arr[value];
+
+    if (value <= 1)
+        return value;
+
+    else
+        arr[value] = fibonacci_euclid(value - 1, arr) + fibonacci_euclid(value - 2, arr);
+
+    return arr[value];
+}
+
 int main()
 {
     int n;
     cin >> n;
-    cout << fibonacci_naive(n);
+    long long arr[n + 1];
+    cout << fibonacci_euclid(n, arr);
 }
