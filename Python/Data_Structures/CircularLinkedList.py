64cb8b5cbd4b8bc6a7c32b5b39494598f2f00485
ArtistBanda <chirag.11gomber4@gmail.com>
2019-12-05 19:53:54 +0530

    Updated CircularLinkedList.py

--- Python/Data_Structures/CircularLinkedList.py
+++ Python/Data_Structures/CircularLinkedList.py
@@ -8,22 +8,54 @@ class CircularLinkedList(object):
     def __init__(self):
         self.last = None
 
+    def addToEmpty(self, value):
+        newNode = Node(value)
+        self.last = newNode
+        self.last.next = newNode
+
     def insertBeginning(self, value):
-        pass
+        if self.last is None:
+            self.addToEmpty(value)
+            return
+        newNode = Node(value)
+        newNode.next = self.last.next
+        self.last.next = newNode
 
     def insertEnd(self, value):
-        pass
+        if self.last is None:
+            self.addToEmpty(value)
+            return
+        newNode = Node(value)
+        newNode.next = self.last.next
+        self.last.next = newNode
+        self.last = newNode
 
-    def deleteEnd(self):
-        return - 1
+    def deleteBeginning(self):
+        if self.last is None:
+            raise ValueError("Underflow")
+        ptr = self.last.next
+        self.last.next = ptr.next
+        del ptr
 
     def displayAll(self):
-        pass
+        ptr = self.last.next
+        while True:
+            print(ptr.value, end=' ')
+            ptr = ptr.next
+            if ptr is self.last:
+                print(ptr.value, end=' ')
+                break
+        print()
+
+# Test Code
 
 
 CLL = CircularLinkedList()
 CLL.insertBeginning(1)
 CLL.insertBeginning(2)
+CLL.insertBeginning(3)
 CLL.displayAll()
-CLL.insertEnd(3)
+CLL.insertEnd(4)
+CLL.displayAll()
+CLL.deleteBeginning()
 CLL.displayAll()
