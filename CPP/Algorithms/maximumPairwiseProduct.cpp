c076e6c53dcabf9f8fbbc82c79bbaba27f82ef97
ArtistBanda <chirag.11gomber4@gmail.com>
2020-01-06 15:22:31 +0530

    added logic code maximumPairwiseProduct.cpp

--- CPP/Algorithms/maximumPairwiseProduct.cpp
+++ CPP/Algorithms/maximumPairwiseProduct.cpp
@@ -4,7 +4,23 @@
 
 int MaxPairwiseProduct(const std::vector<int> &numbers)
 {
-    return 1;
+    int n = numbers.size(), max_index1, max_index2;
+    max_index1 = 0;
+
+    for (int i = 1; i < n; i++)
+        if (numbers[i] > numbers[max_index1])
+            max_index1 = i;
+
+    if (max_index1 == 0)
+        max_index2 = 1;
+    else
+        max_index2 = 0;
+
+    for (int i = 1; i < n; i++)
+        if (numbers[i] > numbers[max_index2] && i != max_index1)
+            max_index2 = i;
+
+    return numbers[max_index1] * numbers[max_index2];
 }
 
 int main()
