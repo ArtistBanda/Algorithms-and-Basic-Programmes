c1e570451c01d66af20b07faecda508116363d46
ArtistBanda <chirag.11gomber4@gmail.com>
2020-01-07 15:24:50 +0530

    added problem desc. maximumPairwiseProduct.cpp

--- CPP/Algorithms/maximumPairwiseProduct.cpp
+++ CPP/Algorithms/maximumPairwiseProduct.cpp
@@ -1,3 +1,18 @@
+/* 
+Maximum Pairwise Product Problem
+Find the maximum product of two distinct num-
+bers in a sequence of non-negative integers.
+
+Input: A sequence of non-negative
+integers.
+
+Output: The maximum value that
+can be obtained by multiplying
+two different elements from the se-
+quence.
+
+*/
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
