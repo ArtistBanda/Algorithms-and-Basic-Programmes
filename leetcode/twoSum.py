fdb50ebb0cb07fdaa5e1a428790945d4495871f7
ArpitSharma2800 <51937952+ArpitSharma2800@users.noreply.github.com>
2021-03-27 17:07:54 +0530

    run the sample input and output

--- leetcode/twoSum.py
+++ leetcode/twoSum.py
@@ -8,8 +8,9 @@
 # Output: [0,1]
 # Output: Because nums[0] + nums[1] == 9, we return [0, 1].
 
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+from typing import List
+
+def twoSum(nums: List[int], target: int) -> List[int]:
         complement = dict()
         for i in range (len(nums)):
             num = nums[i]
@@ -18,3 +19,6 @@ class Solution:
                 return(complement[num],i)
             else:
                 complement[comp] = i
+
+print(twoSum(nums=[2,7,11,15],target= 9))
+
