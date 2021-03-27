27e499bd9cb96b42c33f502cddbc343abf8b5127
ArpitSharma2800 <51937952+ArpitSharma2800@users.noreply.github.com>
2021-03-27 17:10:11 +0530

    indentation resolved

--- leetcode/twoSum.py
+++ leetcode/twoSum.py
@@ -10,15 +10,16 @@
 
 from typing import List
 
+
 def twoSum(nums: List[int], target: int) -> List[int]:
-        complement = dict()
-        for i in range (len(nums)):
-            num = nums[i]
-            comp = target - num 
-            if num in complement:
-                return(complement[num],i)
-            else:
-                complement[comp] = i
+    complement = dict()
+    for i in range(len(nums)):
+        num = nums[i]
+        comp = target - num
+        if num in complement:
+            return(complement[num], i)
+        else:
+            complement[comp] = i
 
-print(twoSum(nums=[2,7,11,15],target= 9))
 
+print(twoSum(nums=[2, 7, 11, 15], target=9))
