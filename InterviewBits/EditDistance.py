a0613d20521d88f35a169947cb5fa7e4e84b3771
ArtistBanda <chirag.codes@gmail.com>
2021-04-06 23:45:02 +0530

    Added EditDistance.py to InterviewBits

---
+++ InterviewBits/EditDistance.py
@@ -0,0 +1,76 @@
+"""
+Given two strings A and B, find the minimum number of steps required to convert A to B. (each operation is counted as 1 step.)
+
+You have the following 3 operations permitted on a word:
+
+Insert a character
+Delete a character
+Replace a character
+
+
+Input Format:
+
+The first argument of input contains a string, A.
+The second argument of input contains a string, B.
+Output Format:
+
+Return an integer, representing the minimum number of steps required.
+Constraints:
+
+1 <= length(A), length(B) <= 450
+Examples:
+
+Input 1:
+    A = "abad"
+    B = "abac"
+
+Output 1:
+    1
+
+Explanation 1:
+    Operation 1: Replace d with c.
+
+Input 2:
+    A = "Anshuman"
+    B = "Antihuman"
+
+Output 2:
+    2
+
+Explanation 2:
+    => Operation 1: Replace s with t.
+    => Operation 2: Insert i.
+"""
+
+def editDistance(str1, str2, m, n):
+ 
+    # If first string is empty, the only option is to
+    # insert all characters of second string into first
+    if m == 0:
+        return n
+ 
+    # If second string is empty, the only option is to
+    # remove all characters of first string
+    if n == 0:
+        return m
+ 
+    # If last characters of two strings are same, nothing
+    # much to do. Ignore last characters and get count for
+    # remaining strings.
+    if str1[m-1] == str2[n-1]:
+        return editDistance(str1, str2, m-1, n-1)
+ 
+    # If last characters are not same, consider all three
+    # operations on last character of first string, recursively
+    # compute minimum cost for all three operations and take
+    # minimum of three values.
+    return 1 + min(editDistance(str1, str2, m, n-1),    # Insert
+                   editDistance(str1, str2, m-1, n),    # Remove
+                   editDistance(str1, str2, m-1, n-1)    # Replace
+                   )
+ 
+ 
+# Driver code
+str1 = "Anshuman"
+str2 = "Antihuman"
+print(editDistance(str1, str2, len(str1), len(str2)))
